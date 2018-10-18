4a1e58ded4d263d1a628895a72e2728114216172
YOUDavid <8342705818ydyttt@gmail.com>
2018-10-18 15:01:46 +0800

    views

--- demo/content/urls.py
+++ demo/content/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path ('login', views.loginPageView, name = 'login'),
+	path ('login.html', views.loginPageView, name = 'login'),
 	path ('admin', views.adminLandingPageView, name = 'admin'),
 	path ('nonadmin', views.nonadminLandingPageView, name = 'nonadmin'),
-	path ('loginsubmit', views.loginSubmitPageView, name = 'loginsubmit')
+	path ('loginsubmit', views.loginSubmitPageView, name = 'loginsubmit'),
+	path ('signupsubmit', views.signupSubmitPageView, name = 'signupsubmit'),
+	path ('home.html', views.homePageView, name = 'home'),
+	path ('signup.html', views.signupPageView, name = 'signup')
 ] 
